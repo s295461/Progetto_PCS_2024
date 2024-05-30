23342b1e1c37c4c3adc19e2b4acf5bbd746dfff3
s295461 <162353653+s295461@users.noreply.github.com>
2024-05-30 11:45:30 +0200

    Update Utils.hpp

--- Project/src/Utils.hpp
+++ Project/src/Utils.hpp
@@ -7,23 +7,21 @@ using namespace std;
 
 namespace FractureNetwork {
 
-bool ImportFracture(const string& filePath, DiscreteFractureNetwork& fracture, Traces trace);
+bool ImportFracture(const string& filePathInput, const string filePathOutput, DiscreteFractureNetwork& fracture, Traces& trace);
 
-bool readFracture(const string& filePath, const string& fileName, DiscreteFractureNetwork& fracture);
+bool ReadFracture(const string& filePath, const string& fileName, DiscreteFractureNetwork& fracture);
 
+bool FractureIntersection(const DiscreteFractureNetwork fracture, Traces& trace);
 
-bool FractureIntersection(const DiscreteFractureNetwork fracture, Traces trace);
+// bool compare(Vector3d a, Vector3d b);
 
-bool compare(Vector3d a, Vector3d b);
+// bool isBetween(Vector3d a, Vector3d b, Vector3d c);
 
-bool isBetween(Vector3d a, Vector3d b, Vector3d c);
+bool FindTraces(const Vector3d s, const Vector3d Point, const DiscreteFractureNetwork& fracture, unsigned int Id1, unsigned int Id2, Traces& trace);
 
-bool findTraces(const Vector3d s, const Vector3d Point, const DiscreteFractureNetwork& fracture, unsigned int Id1, unsigned int Id2, Traces trace);
+void SaveTraces(double n, double m, Vector3d point, Vector3d s, Traces& trace, unsigned int Id1, unsigned int Id2);
 
-BoundingBox BBox3D(const MatrixXd& vertices);
-
-
-bool findTraces(const Vector3d s, const Vector3d Point, const DiscreteFractureNetwork& fracture, unsigned int Id1, unsigned int Id2, Traces trace);
-
+bool PrintOnFile(const string fileName, const string filePath, Traces trace);
 
 }
+
